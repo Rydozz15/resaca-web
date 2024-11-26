89584d8c71178c36817bd0840de94554f3a8adda
Rydozz15 <juan.fuentes@uc.cl>
2024-11-26 00:01:14 -0300

    Fix astro.config duplicate bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), sitemap()],
   site: "https://resacachile.cl/",
-  integrations: [sitemap()],
 });
